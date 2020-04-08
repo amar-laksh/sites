971aeaeb2841d593d2919173f98c8a1c6cd7e96e
amar-laksh <amar.lakshya@protonmail.com>
2020-04-08 13:37:52 +0100

    Doing KRYPTON

--- overthewire/connect.sh
+++ overthewire/connect.sh
@@ -5,15 +5,15 @@ set -o nounset                                  # Treat unset variables as an er
 # Date              : 28.08.2019
 # Last Modified Date: 30.08.2019
 # Last Modified By  : Amar Lakshya <amar.lakshya@xaviers.edu.in>
-WARGAME=narnia
-PORT=2226
+WARGAME=krypton
+PORT=2222
 cur=$(echo "$1 - 1" | bc)
 if   [[ $# -lt 1  ]] || [[ "$cur" -gt "$(cat $WARGAME/flags | wc -l)" ]]; then
 	echo "usage: ./connect.sh level"
 	exit 0
 fi
 echo "So let's go in $WARGAME$cur... to get $WARGAME$1!"
-chmod 600 $WARGAME/*.sshkey &> /dev/null
+#chmod 600 $WARGAME/*.sshkey &> /dev/null
 #firefox http://overthewire.org/wargames/$WARGAME/$WARGAME$1.html
 flag=$(cat $WARGAME/flags | awk -v n=$cur 'FNR == n')
 #if [[ $(echo $flag | grep -c "ssh") -eq 1 ]]; then
